978c30a289d5585d408b7da9c28b1708d7c05cc4
Gvazquez <vazquez.gonzaloemanuel@gmail.com>
2023-03-16 14:08:18 -0300

    commit develop 0.0.7

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,65 @@
 import React from 'react'
 import Navbar from './Navbar';
+import {motion,useScroll,useTransform,useAnimation, easeOut} from 'framer-motion'
 
 import './Css/Header.css';
 
 const Header = () => {
+  const { scrollYProgress } = useScroll()
+  //bgLayer
+  const LayerBG=(props)=>{
+
+    const {src,speed,layer,alt}=props
+    const yscroll=useTransform(scrollYProgress,[0,1],[0,100*speed])
+    if(alt!=="montañas"){
+      return(
+        <motion.img src={src} 
+        className={'headerbg__img '+ layer} 
+        alt={alt}
+        style={{y:yscroll}}></motion.img>
+      )
+    }else{
+      return(
+        <motion.img src={src} 
+        className={'headerbg__img '+ layer} 
+        alt={alt}
+        style={{y:yscroll,x:yscroll}}></motion.img>
+      )
+    }
+    
+
+  }
 
   return (
     <header className="header">
       <Navbar />
       <section className="headerbg">
 
-        <img src='/img/bg/capa4.png' className='headerbg__img layer4' alt='cielo'></img>
-        <img src='/img/bg/capa3.png' className='headerbg__img layer3' alt='montañas'></img>
+        <LayerBG src='/img/bg/capa4.png' speedY={3}  layer="layer4" alt="cielo"/>
+        <LayerBG src='/img/bg/capa33.png' speed={-3} layer="layer3 montains" alt="montañas"/>
+        {/*<img src='/img/bg/capa3.png' className='headerbg__img layer3' alt='montañas'></img>*/}
 
         <div className="headerbg__titulo">
-          <h1 className="titulo1" >GONZALO EMANUEL VAZQUEZ</h1>
+          <h5 className="titulo0" >Mendoza, Argentina</h5>
+          <motion.div className='titulo1-ctnr'
+          initial={{width:5,backgroundColor:"#13B7FF"}}
+          animate={{opacity:[1,0,1,0,1,0,1,0,1], width:'100%',height:122,backgroundColor:"#1c1c1c90"}}
+          transition={{
+            opacity:{duration:2},
+            width:{delay:2,duration:1,ease:easeOut},
+            height:{delay:2.5,duration:0.7,ease:easeOut},
+            backgroundColor:{delay:3.5,duration:1,ease:easeOut}
+            }}>
+          
+            <motion.h1 className="titulo1" 
+            initial={{display:'none',opacity:0}}
+            animate={{display:'block',opacity:1}}
+            transition={{
+              display:{delay:4,duration:0.1,ease:easeOut},
+              opacity:{delay:5,duration:1,ease:easeOut}
+            }}>GONZALO EMANUEL VAZQUEZ</motion.h1>
+          </motion.div>
+          
           <div className="box-titulo2">
             <h2 className="titulo2">FRONT END DEVELOPER</h2>
             <h2 className="titulo2" style={{ opacity: '0.6' }}>FRONT END DEVELOPER</h2>
@@ -29,8 +74,10 @@ const Header = () => {
           <img src='/img/logos/JS.png' className='headerbg__logos__img' alt=''></img>
           <img src='/img/logos/React.png' className='headerbg__logos__img' alt=''></img>
         </div>
-        <img src='/img/bg/capa2.png' className='headerbg__img layer2' alt='edificios'></img>
-        <img src='/img/bg/capa1.png' className='headerbg__img layer1' alt='edificios' ></img>
+        <LayerBG src='/img/bg/capa2.png' speed={-8} layer='layer2' alt='edificios'/>
+        <LayerBG src='/img/bg/capa1.png' speed={-8} layer='layer1' alt='edificios'/>
+        {/*<img src='/img/bg/capa2.png' className='headerbg__img layer2' alt='edificios'></img>
+        <img src='/img/bg/capa1.png' className='headerbg__img layer1' alt='edificios' ></img>*/}
 
       </section>
 
--- src/components/Main/subcomponents/SectionPorfolio.js
+++ src/components/Main/subcomponents/SectionPorfolio.js
@@ -1,31 +1,113 @@
-import React from "react";
+import { motion,useScroll, useTransform,useAnimation} from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import React,{useRef,useEffect} from "react";
 
 import "../Css/SectionPorfolio.css";
 import CarrouselP from "../Items/CarrouselP";
-import DataImg from "../Items/dataImg.json";
 
 const SectionPorfolio = () => {
-  const { images } = DataImg;
+  
+  const { scrollYProgress } = useScroll()
+  //titulo Porfolio
+  const TituloPf=(prop)=>{
+    const {speed}=prop;
+    const xvalue= useTransform(scrollYProgress,[0,0.7,1],[0,70,100*speed])
+    return(
+      <motion.h1 className="sectionPf__h1 layer1" 
+      initial={{opacity:0}} 
+      whileInView={{opacity:1}} 
+      transition={{delay:0.3,duration:1}}
+      style={{x:xvalue}}
+      >PORFOLIO</motion.h1>
+    )
+  }
+  //titulo Porfolio
+  
+  const {ref,inView}=useInView();
+  const fadeInPf=useAnimation();
+  const fadeInPfV=useAnimation();
+
+  useEffect(()=>{
+    if(inView){
+      fadeInPf.start({
+         opacity:1,
+         x:`0%`,
+         transition:{
+          duration:1.3,
+          delay:1
+         }
+      });
+      fadeInPfV.start({
+        opacity:1,
+        x:`0%`,
+        transition:{
+         duration:1.3,
+         delay:1.6
+        }
+     });
+    }else{
+      fadeInPf.start({
+        opacity:0,
+        x:`10%`,
+        transition:{
+         duration:0.3,
+         delay:0
+        }
+     });
+     fadeInPfV.start({
+      opacity:0,
+      x:`-10%`,
+      transition:{
+       duration:0.3,
+       delay:0
+      }
+   });
+    }
+    
+  },[inView]);
+
+//creo el fondo condor parallax scroll
+  const Porfoliobg=(props)=>{
+    const {img,speed}=props;
+   
+    const yValue = useTransform(scrollYProgress,[0,0.7,1],[0,70,100*speed])
+  
+    return(
+      <motion.img
+      className="sectionPf__bg layer3 condor"
+      src={img}
+      alt=""
+      initial={{opacity:0}}
+      transition={{duration:1}}
+      whileInView={{opacity:1}}
+      style={{y:yValue}}></motion.img>
+    )
+  }
+  //creo el fondo condor parallax scroll
+
+  //SECTION PORFOLIO SECTION PORFOLIO
   return (
     <section className="sectionPf">
+      
       <img
         src="/img/bg/cerros.png"
         className="sectionPf__bg layer4"
         alt=""
       ></img>
-      <img
-        src="/img/bg/Condor.png"
-        className="sectionPf__bg condor layer3"
-        alt=""
-      ></img>
-      <h1 className="sectionPf__h1 layer1">PORFOLIO</h1>
+      <Porfoliobg   img="/img/bg/Condor.png" speed={-2}/>
+      <TituloPf speed={2}/>
       <div className="sectionPf__works__ctr">
-        <div className="sectionPf__works">
-          <CarrouselP />
-        </div>
-        <div className="sectionPf-video">
+        <motion.div className="sectionPf__works" ref={ref} 
+        initial={{ opacity: 0, x: `-10%` }}
+        animate={fadeInPf}
+        >
+          <CarrouselP/>
+        </motion.div>
+        <motion.div className="sectionPf-video"
+         initial={{opacity:0,x:`-10%`}}
+         animate={fadeInPfV}>
           <div className="sectionPf-video__screen"></div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
